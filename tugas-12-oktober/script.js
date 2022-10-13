377894ede6e75c4fab0ec29bbcd6a68810168cb7
harispriantury <prianturyharis@gmail.com>
2022-10-13 23:18:12 +0700

    tambah tugas tanggal 12

---
+++ tugas-12-oktober/script.js
@@ -0,0 +1,19 @@
+//deklarasi 3 buah variable
+let angkaPertama = 10;
+let angkaKedua = 20;
+
+//operasi penjumlahan
+let hasil = angkaPertama + angkaKedua;
+console.log(hasil);
+
+// operasi pengurangan 
+hasil = angkaPertama - angkaKedua;
+console.log(hasil);
+
+//operasi pembagian 
+hasil = angkaPertama % angkaKedua;
+console.log(hasil);
+
+//operasi perkalian 
+hasil = angkaPertama * angkaKedua;
+console.log(hasil)
