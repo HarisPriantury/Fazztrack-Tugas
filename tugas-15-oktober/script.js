dbc897a93e8f286ea7dca91e2460a28fd4dd4ef3
harispriantury <prianturyharis@gmail.com>
2022-10-22 17:36:10 +0700

    tambah tugas tanggal 19 oktober

--- tugas-15-oktober/script.js
+++ tugas-15-oktober/script.js
@@ -22,30 +22,30 @@ function ganjilAtauGenap (parameter){
     console.log("Jadi angka "+parameter+" adalah "+bilangan);
 };
 //memanggil function ganjilAtauGenap
-// ganjilAtauGenap(1/2);
+// ganjilAtauGenap(5);
 
 
 
 
-// tugas nomor 2
+// // tugas nomor 2
 
-//deklarasi variable angka
-let angka = 15;
-//menambahkan perulangan atau looping for
-for(let x = 1; x <= angka ; x++){
-    if(x % 3 == 0 && x % 5 == 0 ){
-        console.log("fazztrack");
-    }
-    else if(x % 3 == 0 ){
-        console.log("fazz");
-    }
-    else if(x % 5 == 0 ){
-        console.log("track");
-    }
-    else{
-        console.log(x)
-    }
-};
+// //deklarasi variable angka
+// let angka = 15;
+// //menambahkan perulangan atau looping for
+// for(let x = 1; x <= angka ; x++){
+//     if(x % 3 == 0 && x % 5 == 0 ){
+//         console.log("fazztrack");
+//     }
+//     else if(x % 3 == 0 ){
+//         console.log("fazz");
+//     }
+//     else if(x % 5 == 0 ){
+//         console.log("track");
+//     }
+//     else{
+//         console.log(x)
+//     }
+// };
 
 
 
